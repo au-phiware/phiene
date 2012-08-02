ed2edf4a453f8cabf8b2727cb829e47c51426036
Corin Lawson <corin@phiware.com.au>
2012-08-02 23:57:05 +1000

    Tests TODO

---
+++ test/au/com/phiware/ga/containers/PloidTest.java
@@ -0,0 +1,19 @@
+package au.com.phiware.ga.containers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PloidTest {
+
+	@Test
+	public void testWriteGenome() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testReadGenome() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
