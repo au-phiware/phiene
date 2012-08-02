305d41290a89c828a771ec7881613089737f4a7f
Corin Lawson <corin@phiware.com.au>
2012-08-02 23:34:24 +1000

    Merge multiple chromosomes. No errors trumps single errors; double errors zero-out. Decoded bits are ORed unless trumped.

--- src/au/com/phiware/ga/io/ChromosomeInputStream.java
+++ src/au/com/phiware/ga/io/ChromosomeInputStream.java
@@ -1,5 +1,6 @@
 package au.com.phiware.ga.io;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,18 +23,29 @@ public class ChromosomeInputStream extends FilterInputStream {
 		1 << 0, 1 << 4, 1 << 6, 1 << 2, 1 << 7, 1 << 3, 1 << 5, 1 << 1
 	};
 	private OutputStream syndrome;
-	
+	private int size = 1; 
+
+	public ChromosomeInputStream(InputStream in) {
+		this(in, new ByteArrayOutputStream(), 1);
+	}
+	public ChromosomeInputStream(InputStream in, int size) {
+		this(in, new ByteArrayOutputStream(), size);
+	}
 	public ChromosomeInputStream(InputStream in, OutputStream err) {
-		super(in);
-		syndrome = err;
+		this(in, err, 1);
 	}
 
+	public ChromosomeInputStream(InputStream in, OutputStream err, int size) {
+		super(in);
+		syndrome = err;
+		this.size = size;
+	}
 	public int read()
 	         throws IOException {
-		int[] clear = {0, 0};
-		byte[] s = {0, 0};
-		byte[] coded = {0, 0};
-		int x, p;
+		int[] clear = new int[size << 1];
+		byte[] s = new byte[size << 1];
+		byte[] coded = new byte[size << 1];
+		int x, p, b = 0;
 		
 		if (in.read(coded) < 0)
 			return -1;
@@ -51,9 +63,23 @@ public class ChromosomeInputStream extends FilterInputStream {
 			clear[i] = ((coded[i] & 32) >>> 1 | coded[i] & 14) >>> 1;
 		}
 		
-		syndrome.write(s);
+		for (int j = 0; j < 2; j++) {
+			boolean found = false;
+			b <<= 4;
+			for (int i = 0; i < size; i++) {
+				if (s[j * size + i] == 0) {
+					if (!found) b &= ~0xF;
+					else found = true;
+					b |= clear[j * size + i];
+				} else if (!found)
+					b |= clear[j * size + i];
+			}
+		}
 		
-		return clear[1] | (clear[0] << 4);
+		syndrome.write(s);
+		syndrome.flush();
+		
+		return b;
 	}
 	
 	public int read(byte[] b,
@@ -68,7 +94,7 @@ public class ChromosomeInputStream extends FilterInputStream {
 		}
 		return i - off;
 	}
-
+	
 	public OutputStream getSyndrome() {
 		return syndrome;
 	}
